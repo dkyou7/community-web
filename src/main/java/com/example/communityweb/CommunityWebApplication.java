fa783a4510a0beb9d67f758854c693526fcc7002
youdongkwan <dkyou7@naver.com>
2020-11-16 17:38:58 +0900

    4.3.5 CommandLineRunner 를 사용하여 DB에 데이터 넣기

--- src/main/java/com/example/communityweb/CommunityWebApplication.java
+++ src/main/java/com/example/communityweb/CommunityWebApplication.java
@@ -1,7 +1,17 @@
 package com.example.communityweb;
 
+import com.example.communityweb.domain.Board;
+import com.example.communityweb.domain.User;
+import com.example.communityweb.domain.enums.BoardType;
+import com.example.communityweb.repository.BoardRepository;
+import com.example.communityweb.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class CommunityWebApplication {
@@ -10,4 +20,20 @@ public class CommunityWebApplication {
 		SpringApplication.run(CommunityWebApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception{
+		return (args) -> {
+			User user = userRepository.save(User.builder()
+					.name("test").password("test").email("test@naver.com").createdDate(LocalDateTime.now()).build());
+
+			IntStream.rangeClosed(1,200).forEach(index -> boardRepository.save(
+					Board.builder().title("title"+index)
+					.content("content"+index)
+					.boardType(BoardType.free)
+					.createdDate(LocalDateTime.now())
+					.updatedDate(LocalDateTime.now())
+					.user(user).build()
+			));
+		};
+	}
 }
